9c76f936a8c484da72b79b70a19e9cf9f3ad9d34
Deep Thought <harpreetset@gmail.com>
2026-06-09 11:38:55 -0700

    Delete TTS directory

--- TTS/front-end/config.rb
+++
@@ -1,9 +0,0 @@
-preferred_syntax = :scss
-http_path = '/'
-css_dir = 'css'
-scss_dir = 'scss'
-images_dir = 'images'
-javascripts_dir = 'js'
-relative_assets = true
-line_comments = true
-# output_style = :compressed
--- TTS/views.py
+++
@@ -1,9 +0,0 @@
-
-# Create your views here.
-'''
-Created on Oct 4, 2016
-
-@author: hsethi
-'''
-#from django.shortcuts import render
-#placeholder
